fa759d24a640cb4b69a87dfa62997e5fb138e695
Hanson Yu <hyu@better.com>
2020-03-30 12:26:05 -0400

    try nil instead of 0

--- z3/optimize.go
+++ z3/optimize.go
@@ -243,7 +243,7 @@ func (o *Optimize) Minimize(val Value) *Objective {
 func (o *Optimize) Check() (sat bool, err error) {
 	var res C.Z3_lbool
 	o.ctx.do(func() {
-		res = C.Z3_optimize_check(o.ctx.c, o.o, 0, 0)
+		res = C.Z3_optimize_check(o.ctx.c, o.o, 0, nil)
 	})
 	if res == C.Z3_L_UNDEF {
 		// Get the reason
